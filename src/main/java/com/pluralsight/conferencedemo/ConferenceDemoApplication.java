acad144e326453d8490caa7a154d36a305619e1d
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2020-04-12 01:52:43 +0530

    Updated application.properties (error debug)

--- src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -1,7 +1,9 @@
 package com.pluralsight.conferencedemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ConferenceDemoApplication {
