0516ef35d3cabd6b61be9ad6a0db97d0eb0f88b0
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2020-04-12 00:49:26 +0530

    Added session controller with CRUD MVC

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
@@ -0,0 +1,28 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionController {
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list(){
+        return  sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id){
+        return sessionRepository.getOne(id);
+    }
+
+}
