5a5da8bd59955272c21e971104b90eed780b83ae
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2020-04-12 01:16:10 +0530

    Added session CRUD

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
@@ -2,6 +2,7 @@ package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +12,11 @@ import java.util.List;
 @RequestMapping("/api/v1/sessions")
 public class SessionController {
 
-    @Autowired
-    private SessionRepository sessionRepository;
+    private final SessionRepository sessionRepository;
+
+    public SessionController(SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
 
     @GetMapping
     public List<Session> list(){
@@ -25,4 +29,24 @@ public class SessionController {
         return sessionRepository.getOne(id);
     }
 
+    @PostMapping
+    public Session create(@RequestBody Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        // TODO: Need to check for children records before deleting(cascading)
+        // Eg. IF Session has children records, foreign key constraint violation
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session){
+        // because this is a PUT method, we expect all attributes to be passed in. A patch will only need what needs to be updated
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
 }
---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -0,0 +1,33 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakerController {
+
+    private final SpeakerRepository speakerRepository;
+
+    public SpeakerController(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
+    }
+
+    @GetMapping
+    public List<Speaker> list(){
+        return  speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+}
