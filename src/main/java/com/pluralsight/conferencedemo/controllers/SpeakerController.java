ddada8ee205a02171f7512f2ba96aaf0e946f64e
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2020-04-12 01:22:27 +0530

    Added speaker CRUD

--- src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -1,12 +1,11 @@
 package com.pluralsight.conferencedemo.controllers;
 
+import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.models.Speaker;
 import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +28,25 @@ public class SpeakerController {
     @RequestMapping("{id}")
     public Speaker get(@PathVariable Long id){
         return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        // TODO: Need to check for children records before deleting(cascading)
+        // Eg. IF Speaker has children records, foreign key constraint violation
+        speakerRepository.deleteById(id);
+    }
+
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        // because this is a PUT method, we expect all attributes to be passed in. A patch will only need what needs to be updated
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
     }
 }
